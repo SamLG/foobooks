a079408d47aa4d6f38f6969df15c3b38051c4944
Samantha Grise <AndrewMartin@AndrewMartin.local>
2016-10-21 13:21:22 -0400

    update routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,45 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/example', function() {
+//     return App::environment();
+// });
+// Route::get('/books', 'BookController@index')->name('books.index');
+
+# View all books
+Route::get('/books', function() {
+
+})->name('books.index');
+
+# Display form to add a new book
+Route::get('/books/create', function() {
+
+})->name('books.create');
+
+# Process form to add a new book
+Route::post('/books', function() {
+
+})->name('books.store');
+
+# Display an individual book
+Route::get('/books/{book}', function($book) {
+
+})->name('books.show');
+
+# Display form to edit an individual book
+Route::get('/books/{book}/edit', function($book) {
+
+})->name('books.edit');
+
+# Process form to save edits to an individual book
+Route::put('/books/{book}', function($book) {
+
+})->name('books.update');
+
+# Delete an individual book
+Route::delete('/books/{book}', function($book) {
+
+})->name('books.destroy');
